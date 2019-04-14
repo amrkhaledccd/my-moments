2ee929a0b38bceb3e27653cc70c0b14b12b77e58
Amr Khaled <amrkhaledccd@hotmail.com>
2019-04-14 21:10:12 +0200

    add route to post service

--- insta-post-service/src/main/java/com/clone/instagram/instapostservice/exception/NotAllowedException.java
+++ insta-post-service/src/main/java/com/clone/instagram/instapostservice/exception/NotAllowedException.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 public class NotAllowedException extends RuntimeException {
 
     public NotAllowedException(String user, String resource, String operation) {
-        super(String.format("user %s is not allowed to % resource %s",
+        super(String.format("user %s is not allowed to %s resource %s",
                 user, operation, resource));
     }
 }
