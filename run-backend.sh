eedacf1e2bca53be7de8f703abd868fd2f38a76d
Amr Khaled <amrkhaledccd@hotmail.com>
2019-05-24 01:56:22 +0200

    fix key serialization

--- run-backend.sh
+++ run-backend.sh
@@ -1,12 +1,12 @@
 cd ./insta-discovery 
 ./run.sh &
-cd ../auth-service 
-./run.sh &
+#cd ../auth-service 
+#./run.sh &
 cd ../insta-api-gateway
 ./run.sh &
 cd ../insta-media-service
 ./run.sh &
-#cd ../insta-post-service
-#./run.sh &
+cd ../insta-post-service
+./run.sh &
 cd ../insta-graph-service
 ./run.sh
