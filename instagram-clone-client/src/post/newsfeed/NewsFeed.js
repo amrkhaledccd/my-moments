86e2c2e70974850a5df356b22466bc82f61bc4d1
Amr Khaled <amrkhaledccd@hotmail.com>
2019-06-05 23:55:00 +0200

    implement feed user profile in client app

--- instagram-clone-client/src/post/newsfeed/NewsFeed.js
+++ instagram-clone-client/src/post/newsfeed/NewsFeed.js
@@ -1,18 +1,41 @@
 import React, { Component } from "react";
+import "./newsfeed.css";
+import { Row, Col, Avatar } from "antd";
+import { ACCESS_TOKEN } from "../../common/constants";
 
 class NewsFeed extends Component {
   state = { currentUser: { ...this.props.currentUser } };
 
   componentDidMount = () => {
-    if (!this.props.isAuthenticated) {
+    if (!localStorage.getItem(ACCESS_TOKEN)) {
       this.props.history.push("/login");
     }
   };
 
   render() {
     return (
-      <div>
-        <h1>NewsFeed: {this.state.currentUser.username} </h1>
+      <div className="feed-container">
+        <Row>
+          <Col span={16}>Hello there</Col>
+          <Col span={8}>
+            <Row>
+              <Col span={6}>
+                <div className="user-avatar">
+                  <Avatar
+                    src={this.state.currentUser.profilePicture}
+                    className="feed-user-avatar-circle"
+                  />
+                </div>
+              </Col>
+              <Col className="feed-user-details-container" span={18}>
+                <div className="feed-username">
+                  {this.state.currentUser.username}
+                </div>
+                <div className="feed-name">{this.state.currentUser.name}</div>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
       </div>
     );
   }
