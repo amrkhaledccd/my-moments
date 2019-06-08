278099a9ad591b0e12c2f49283d53cf617287f7e
Amr Khaled <amrkhaledccd@hotmail.com>
2019-06-08 15:40:34 +0200

    change image urls

--- instagram-clone-client/src/post/newsfeed/PostList.js
+++ instagram-clone-client/src/post/newsfeed/PostList.js
@@ -35,23 +35,23 @@ class PostList extends Component {
       {
         username: "mosalah",
         imageUrl:
-          "http://amr-hp-probook-450-g2:8000/images/amrkhaled/ed03dc30-26fa-4298-bfff-c3557ac32523.jpg",
+          "https://scontent-ber1-1.cdninstagram.com/vp/4134f3fda8274d81d2b9a230363ab726/5D9CAA3E/t51.2885-15/e35/p1080x1080/60962985_711959852552300_2236926061042845839_n.jpg?_nc_ht=scontent-ber1-1.cdninstagram.com",
         userPic:
-          "http://amr-hp-probook-450-g2:8000/images/mosalah/7935f63b-d2b5-430e-9f7c-9cfd9a33f49e.jpg"
+          "https://scontent-ber1-1.cdninstagram.com/vp/b0a604cf102c8c80050f80e5adb5bc28/5D784DBD/t51.2885-19/s150x150/61448296_852636165097426_3602678044591915008_n.jpg?_nc_ht=scontent-ber1-1.cdninstagram.com"
       },
       {
         username: "nicole",
         imageUrl:
-          "http://amr-hp-probook-450-g2:8000/images/nicole/3a9aaeb4-0266-473a-a10c-399fcde6da0c.jpg",
+          "https://scontent-ber1-1.cdninstagram.com/vp/0516d2a92728efe36745da33a2b2a80d/5D85C338/t51.2885-15/sh0.08/e35/s640x640/62407036_141431193672906_7348878471045387426_n.jpg?_nc_ht=scontent-ber1-1.cdninstagram.com",
         userPic:
-          "http://amr-hp-probook-450-g2:8000/images/nicole/18baff93-6511-470c-852e-e39cd0d900ae.png"
+          "https://scontent-ber1-1.cdninstagram.com/vp/7056d98b38fbb4385aa21db6ab0a8ad3/5D8DD45F/t51.2885-19/s150x150/16465350_1655196611444099_5930266198347350016_a.jpg?_nc_ht=scontent-ber1-1.cdninstagram.com"
       },
       {
         username: "josefrakich",
         imageUrl:
-          "http://amr-hp-probook-450-g2:8000/images/josefrakich/2fdf2d8a-89ff-4f92-8adf-b2f79366e161.jpg",
+          "https://scontent-ber1-1.cdninstagram.com/vp/96f47fe1d56eb213965f87e0998f7ac0/5D9200C4/t51.2885-15/e35/p1080x1080/61161464_1419345244896650_279110253350909451_n.jpg?_nc_ht=scontent-ber1-1.cdninstagram.com",
         userPic:
-          "http://amr-hp-probook-450-g2:8000/images/josefrakich/7f164a1f-4afa-4814-84b7-43b1578d071b.jpg"
+          "https://scontent-ber1-1.cdninstagram.com/vp/4c73211a69f4f4d670de1c4e4580c604/5D9E99C7/t51.2885-19/s150x150/30591366_2555910804633524_5260868190619041792_n.jpg?_nc_ht=scontent-ber1-1.cdninstagram.com"
       }
     ];
 
